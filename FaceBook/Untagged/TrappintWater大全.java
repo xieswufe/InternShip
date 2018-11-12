f12be0a6031250ab3f893f74cece8855e95d1c12
Xie_SWUFE <xieswufe@gmail.com>
2018-11-12 08:55:54 -0500

    Update and rename TrappintWater(-1) to TrappintWater大全.java

--- FaceBook/Untagged/TrappintWater大全.java
+++ FaceBook/Untagged/TrappintWater大全.java
@@ -1,3 +1,6 @@
+
+
+// 漏水和不漏水都可以用下面这种写法  time complexity O(n)
 public int trappingWater(int[] nums) {
         if (nums == null || nums.length <= 2) {
             return 0;
@@ -25,4 +28,31 @@ public int trappingWater(int[] nums) {
             }
         }
         return totalwater;
+    }
+
+// two pointer + 每个bar还有宽度
+public int trap(int[] height,int[] bar) {
+        // check corner cases 
+        if (height == null || height.length == 0) {
+            return 0;
+        }
+        // two pointer
+        int left = 0;
+        int right = height.length - 1;
+        int left_max = 0;
+        int right_max = 0;
+        int trap_water = 0;
+        while (left < right) {
+            if (height[left] <= height[right]) {
+                left_max = Math.max(left_max,height[left]);
+               // trap_water += left_max - height[left++];
+                trap_water += bar[left] * (left_max - height[left++]); 
+            }
+            else {
+                right_max = Math.max(right_max,height[right]);
+              //trap_water += left_max - height[right++];
+                trap_water += bar[right] * (right_max - height[right--]);
+            }
+        }
+        return trap_water;
     }
