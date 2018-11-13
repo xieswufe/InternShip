4f54344813b130567f84265870e8317de3b8a389
Xie_SWUFE <xieswufe@gmail.com>
2018-11-13 02:04:10 -0500

    Update meeting rooms.java

--- FaceBook/Untagged/2nd/meeting rooms.java
+++ FaceBook/Untagged/2nd/meeting rooms.java
@@ -1,3 +1,69 @@
+// 正常版本 
+public int minMeetingRooms(Interval[] intervals) {
+        if (intervals.length == 0) return 0;
+        Arrays.sort(intervals, (a,b) -> (a.start - b.start));
+        PriorityQueue<Interval> minheap = new PriorityQueue<>((a,b) -> (a.end - b.end));
+        int res = 0;
+        for (int i = 0; i < intervals.length; i++) {
+            Interval curt = intervals[i];
+            if (!minheap.isEmpty() && minheap.peek().end <= curt.start) {
+                minheap.poll();
+            }
+            minheap.offer(curt);
+            res = Math.max(res, minheap.size());
+        }
+        return res;
+    }
+
+////scan line
+private class Point {
+        int pos;
+        boolean using;
+        
+        private Point(int pos, boolean using) {
+            this.pos = pos;
+            this.using = using;
+        }
+    }
+    public int minMeetingRooms(Interval[] intervals) {
+        // check corner cases 
+        if (intervals  == null || intervals.length == 0) {
+            return 0;
+        }
+        List<Point> points = new ArrayList<Point>();
+        for (Interval item : intervals) {
+            points.add(new Point(item.start,true));
+            points.add(new Point(item.end,false));
+        }
+        Collections.sort(points, new Comparator<Point>() {
+           public int compare(Point p1, Point p2) {
+               if (p1.pos == p2.pos) {
+                   if (p1.using) {
+                       return 1;
+                   }
+                   else {
+                       return -1;
+                   }
+               }
+               return p1.pos - p2.pos;
+           } 
+        });
+        int maxRoom = 0;
+        int curRoom = 0;
+        for (int i = 0; i < points.size(); i++) {
+            if (points.get(i).using) {
+                curRoom ++;
+            }
+            else {
+                curRoom --;
+            }
+            maxRoom = Math.max(curRoom,maxRoom);
+        }
+        return maxRoom;
+    }
+
+////////////
+// 带房间号码
 private  static class Room {
     Interval timeSlot;
     int roomNum;
